217081f882fc33bdd1fee3a0da37472777458c1b
tavoasenjo <tavoasenjo@gmail.com>
2015-06-02 15:01:10 -0400

    Layout-1

--- ComicWorld/resources/views/home.blade.php
+++ ComicWorld/resources/views/home.blade.php
@@ -2,9 +2,40 @@
 
 @section('content')
 
-
-	<div>
-		<p>Marvel</p>
+	<div class="row fullWidth">
+		
+			<img class="captain"src="img/captain3.png">
+			<img class="iron" src="img/arm.png" />
+			<img class="hulk" src="img/hulk.png">
+		
 	</div>
+
+	<div class="row">
+	    <div class="large-7 columns large-centered search_home">
+	      <div class="row collapse">
+	        <div class="small-9 columns">
+	          <input type="text" placeholder="Search">
+	        </div>
+	        <div class="small-3 columns">
+	          <a href="#" class="button postfix">Search</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="row">
+	  	<div class="large-12 columns">
+	  		<table >
+	  			<tr>
+	  				<td class="large-4"></td>
+	  				<td class="large-4"></td>
+	  				<td class="large-4"></td>
+	  			</tr>
+	  		</table>
+
+	  	</div>
+	  	</div>
+	  </div>
+	
 
 @endsection
